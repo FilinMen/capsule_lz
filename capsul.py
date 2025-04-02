2a096ae6ebf0daf3cb3d8a70b108e92d1b5a3ab4
FilinMen <fiilinkadabra@tutamail.com>
2025-04-02 17:25:32 +0300

    good

--- capsul.py
+++ capsul.py
@@ -3,45 +3,54 @@ from datetime import date, datetime
 import pandas as pd
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+from log import log
 
+class Weather:
 
-def log(func):
-    def wrapper(*args, **kwargs):
-        original_result = func(*args, **kwargs)
-        
-        user_name = getpass.getuser()
-        func_name = func.__name__
-        formatted_date = date.today().strftime("%d-%m-%Y")
-        formatted_time = datetime.now().time()
+    @log
+    def __init__(self):
+        self
 
-        if os.path.isfile("logs.csv"):
-            print("Файл существует")
-            file_df = pd.read_csv("logs.csv")
-            df = pd.DataFrame([len(file_df),user_name, func_name,formatted_date, formatted_time],
-            columns=['id','user_name', 'func_name', 'formatted_date', 'data_time'])
-            df.to_csv('logs.csv', mode='a', index=False)
+    def day(self):
 
-        else:
-            print("Файл не существует")
-            df = pd.DataFrame([0 ,user_name, func_name,formatted_date, formatted_time],
-            columns=['id','user_name', 'func_name', 'formatted_date', 'data_time'])
-            df.to_csv('logs.csv')
+        df = pd.read_csv('heavy.csv', usecols= ['MONTH','BASEL_sunshine'])
+        names = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
+        for i in range(1,len(names)+1):
+            df['MONTH'] = df['MONTH'].replace(i, names[i-1]) # Замена числовых месяцев на названия
+        df = df.groupby('MONTH')['BASEL_sunshine'].sum()
 
-        return original_result
-    return wrapper
+        fig, ax = plt.subplots()
+        ax = df.plot(kind='barh', x='MONTH', y='BASEL_sunshine', title='number of sunny days per year', color='white')# Построение горизонтальной диаграммы
 
-class CaesarCipper:
+        fig.patch.set_facecolor('#0000B2')  # Фон всей области графика
+        ax.set_facecolor('#0000B2')     
 
-    def __init__(self,):
-        
+        for i, v in enumerate(df): #текст с количеством рядом с каждым столбцом
+            ax.text(v + 1, i, str(v), color='white', va='center', fontsize=10)
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.tick_params(colors='white')  # Цвет отметок на осях
+        ax.xaxis.label.set_color('white')  # Цвет подписи оси X
+        ax.yaxis.label.set_color('white')  # Цвет подписи оси Y
+        ax.title.set_color('white')        # Цвет заголовка
+        # Добавляем подписи к осям
+        plt.xlabel('day')
+        plt.ylabel('month')
+        # Plot formatting
+        plt.legend()
+        plt.show()
 
     def __del__(self): #деструктор
         print("del done")
 
 
 def main(): 
-    
+
+    weather = Weather()
+    weather.day()
 
 if __name__ == "__main__":
     main()
